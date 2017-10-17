fbdd70b0bca08be87efce14df385abdf46679750
yeyang <ljsavior@163.com>
2017-10-17 23:22:04 +0800

    demo程序

---
+++ src/main/java/com/eternal/Application.java
@@ -0,0 +1,16 @@
+package com.eternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lijun on 17-9-9.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
