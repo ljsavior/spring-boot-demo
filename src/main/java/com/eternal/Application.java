700f7e2853e284f440560acc26f148756ff5a935
yeyang <ljsavior@163.com>
2017-10-18 23:45:02 +0800

    使用fastjson解析json数据

--- src/main/java/com/eternal/Application.java
+++ src/main/java/com/eternal/Application.java
@@ -1,7 +1,13 @@
 package com.eternal;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * Created by lijun on 17-9-9.
@@ -11,6 +17,16 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        converter.setFastJsonConfig(config);
+        HttpMessageConverter<?> httpMessageConverter = converter;
+        return new HttpMessageConverters(httpMessageConverter);
     }
 
 }
