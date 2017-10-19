ff453a6f7c2bbd8d9cef86316341f8696641bad5
夜阳 <yeyang.lj@alibaba-inc.com>
2017-10-19 20:10:51 +0800

    使用druid数据库连接池

--- src/main/java/com/eternal/Application.java
+++ src/main/java/com/eternal/Application.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
@@ -13,6 +14,7 @@ import org.springframework.http.converter.HttpMessageConverter;
  * Created by lijun on 17-9-9.
  */
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/eternal/schedule/PrintTask.java
+++ src/main/java/com/eternal/schedule/PrintTask.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,10 +16,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 public class PrintTask {
-    ThreadLocal<DateFormat> dateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ThreadLocal<DateFormat> dateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 
     @Scheduled(cron = "0/20 * * * * ?") //每20s执行一次
     public void print() {
-        System.out.println(dateFormat.get().format(new Date()));
+        logger.info(dateFormat.get().format(new Date()));
     }
 }
